1b05bb3f297ad50b67354055ff9f04420c766b28
Cincelor <133509193+Timur072604@users.noreply.github.com>
2024-05-19 22:47:10 +0800

    Laboratory 1

--- fibonacci.c
+++ fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 #include "function.h"
 int p[2];  // Массив для хранения дескрипторов канала для чтения и записи
 int o[2];  // Массив для хранения дескрипторов канала для чтения и записи
@@ -11,18 +11,18 @@ void run_fib(){
     size_t sz = read(p[0], &m, sizeof(m));  // Прочитать из канала значение m
     close(p[0]);  // Закрыть дескриптор канала для чтения
     if (sz!=sizeof(m)){
-        printf("Cannot read all the data\n");
+        printf("Unable to access all the information\n");
         _exit(1);  // Выйти из процесса с кодом 1
     }
     size_t res = fibonacci(5);  // Вычислить значение res
-    printf("(SUBPROCESS) Fibonacci number %ld is %ld\n", m, res);  // Вывести значение m и res
+    printf("The Fibonacci number %ld is equivalent to %ld\n", m, res);  // Вывести значение m и res
     close(o[0]);  // Закрыть дескриптор канала для чтения
     write(o[1], &res, sizeof(res));  // Записать в канал значение res
     close(o[1]);  // Закрыть дескриптор канала для записи
 }
 size_t n = 5;
 int main(){
-    printf("Hello, World!\n");
+    printf("Credit the laboratory, please\n");
     size_t cpid;
     pipe(p);  // Создать канал для чтения и записи
     pipe(o);  // Создать канал для чтения и записи
@@ -37,12 +37,12 @@ int main(){
         close(o[1]);  // Закрыть дескриптор канала для записи
         read(o[0], &rc, sizeof(rc);  // Прочитать из канала значение rc
         close(o[0]);  // Закрыть дескриптор канала для чтения
-        printf("Fibonacci number %ld is %ld, which was taken from child process)\n", n, rc);  // Вывести значение n и rc
+        printf("The Fibonacci number %ld is %ld, which was obtained from the child process\n", n, rc);  // Вывести значение n и rc
     }
     if (cpid!=0){
         int rc;
         waitpid(cpid, &rc, 0);  // Дождаться завершения нового процесса
-        printf("A Child process killed.\n");  // Вывести сообщение
+        printf("The child process was terminated\n");  // Вывести сообщение
     }
     return 0;
 }
